915e9a9e3e46aa61a0765161795658f23052fc56
wlsdydlee <eagie0529@naver.com>
2025-08-23 09:29:35 +0900

    jwtConfig

--- src/main/java/com/springboot/lococo/config/JwtConfig.java
+++ src/main/java/com/springboot/lococo/config/JwtConfig.java
@@ -4,11 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.client.RestTemplate;
+
 
 @Configuration
 @RequiredArgsConstructor
 public class JwtConfig {
+
     @Bean
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
